77d9f965ddce505c8b632b8605f111831dfe7968
MichaelEjiofor <odejamichael1999@gmail.com>
2022-10-15 03:54:39 -0700

    put.c

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <stdio>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"programming is like building a multi
-			lingual puzzle");
-	             return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 
-}		     
-		
-
+}
 
