52ea961b64037d3b7ec6c146cd4afef0f63e098b
MichaelEjiofor <odejamichael1999@gmail.com>
2022-10-15 04:30:41 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Entry Point
  *
- * Return: Always 0 (Success)
+ * Returns: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a muiltilingual puzzle");
 	return (0);
 
 }
+
+
