dca400372cc4874e59046367429dee30ffa29262
MichaelEjiofor <odejamichael1999@gmail.com>
2022-10-15 16:47:35 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - print a c program using printf
  *
  * Return: 0 (Success)
  */
 
 int main(void)
-{	printf("with proper grammar, but the outcome is a piece of art,");
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
 }
